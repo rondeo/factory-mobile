df3b3e8e5a197b20f62a39a536e9764bfddd8c41
coco <wosxieez@qq.com>
2019-06-18 17:29:32 +0800

    界面优化

--- src/view/home/HomeView.jsx
+++ src/view/home/HomeView.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Grid } from 'antd-mobile'
+import { Grid, NavBar, WhiteSpace } from 'antd-mobile'
 import Icon1 from '../../assets/1.svg'
 
 const list = ['巡检平台', '缺陷管理', '仓库管理', '工卡管理', '工作票', '安全管理', '视频监控']
 
 export default props => {
-    const data = list.map(item => ({ text: item, icon: <img width='60' height="60" src={Icon1} alt=""></img>}))
-    return <Grid data={data} columnNum={3} hasLine onClick={(el, index) => {
-        if (index === 1) {
-            props.history.push('/bug')
-        } else if (index === 0) {
-            props.history.push('/device')
-        }
-    }} />
+    const data = list.map(item => ({ text: item, icon: <img width='60' height="60" src={Icon1} alt=""></img> }))
+    return <div>
+        <NavBar mode="light">首页</NavBar>
+        <WhiteSpace/>
+        <Grid data={data} columnNum={3} hasLine onClick={(el, index) => {
+            if (index === 1) {
+                props.history.push('/bug')
+            } else if (index === 0) {
+                props.history.push('/device')
+            }
+        }} />
+    </div>
 }
